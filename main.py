a845f1b6733a64a8ab19044eb30811365eecf330
verdenkaa <nikitapahomov97@gmail.com>
2022-04-04 08:19:12 +0300

    add adding musik
    Тестовая версия добавления музыки в дб

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 import telebot
+import requests
+import sqlite3
 from telebot import types
 
 with open("API_KEY", "r") as f:
     __KEY__ = f.readline()
 
+URL = "https://api.telegram.org/bot"
 bot = telebot.TeleBot(__KEY__)
+con = sqlite3.connect("db/music.db", check_same_thread=False)
+cur = con.cursor()
 
 users_step = {}
 
@@ -15,7 +20,7 @@ text = types.KeyboardButton("Текст")
 back_button = types.KeyboardButton("Назад")
 qr_button = types.KeyboardButton("QR код")
 
-@bot.message_handler(content_types=['text', "start"])
+@bot.message_handler(content_types=["text", "start"])
 def main(message):
 
     if (message.text == "/start" or message.text == "Назад"):
@@ -45,7 +50,6 @@ def main(message):
         markup.row(back_button, qr_button)
         bot.send_message(message.chat.id,
                          text="{0.first_name}, Выбери формат поиска".format(message.from_user), reply_markup=markup)
-        print(users_step)
 
     elif (message.text == "Голос"):
 
@@ -56,13 +60,36 @@ def main(message):
         bot.send_message(message.chat.id,
                          text="{0.first_name}, жду голосовой".format(message.from_user), reply_markup=markup)
 
+    elif users_step[message.from_user.id] == "musick_add":
+        users_step[message.from_user.id] = ["musick_add-image", message.text]
+
+
+
+
+
+
 @bot.message_handler(content_types=['photo'])
 def image(message):
-    print("get")
-    if users_step[message.from_user.id] == "musick_find":
+    print(users_step, "image")
+    if users_step[message.from_user.id][0] == "musick_add-image":
         file = message.photo[-1].file_id
-        print(file)
-        #bot.send_message(message.chat.id, te)
+        users_step[message.from_user.id].append(str(file))
+        users_step[message.from_user.id][0] = "musick_add-file"
+
+@bot.message_handler(content_types=['audio'])
+def doc(message):
+    print(users_step, "doc")
+    if users_step[message.from_user.id][0] == "musick_add-file":
+        file = str(message.audio.file_id)
+        cur.execute(f"""INSERT INTO songs VALUES('{users_step[message.from_user.id][2]}',
+                    '{users_step[message.from_user.id][1]}',
+                    '{file}', 'No text')""")
+        con.commit()
+        print("ok")
+
+
+def send_photo_file_id(chat_id, file_id):
+    requests.get(f'{URL}{__KEY__}/sendPhoto?chat_id={chat_id}&photo={file_id}')
 
 
 bot.polling(none_stop=True, interval=1)
