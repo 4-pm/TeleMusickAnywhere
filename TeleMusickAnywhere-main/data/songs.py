838eb62e04117ea8ba3d681bcc73f8e67db2a332
Legion2185 <83009365+lUniverseGameplay@users.noreply.github.com>
2022-04-20 12:29:35 +0300

    add_immutable_files

---
+++ TeleMusickAnywhere-main/data/songs.py
@@ -0,0 +1,16 @@
+import sqlalchemy
+
+from .db_session import SqlAlchemyBase
+
+
+class Song(SqlAlchemyBase):  # класс добавляемойс трочки
+    __tablename__ = 'songs'
+
+    id = sqlalchemy.Column(sqlalchemy.Integer,
+                           primary_key=True, autoincrement=True)  # генерируемый id
+    name = sqlalchemy.Column(sqlalchemy.String, index=True)  # название песни
+    qr = sqlalchemy.Column(sqlalchemy.String)  # ее qr, поменять на блоб
+    gif = sqlalchemy.Column(sqlalchemy.String)  # фоточка, тут лежит гифка с диском
+    song = sqlalchemy.Column(sqlalchemy.String)  # id песни
+    text = sqlalchemy.Column(sqlalchemy.String, index=True)  # текст песни
+    author = sqlalchemy.Column(sqlalchemy.String)  # id автора
